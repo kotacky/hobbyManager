be101346dc70c9938ac8a4ad1aa3dd5ba114f365
WISS1_0067 <WISS1_0067@PC0067>
2015-06-19 11:25:09 +0900

    G0021Control6/18のコードをアップデート_高橋 20150619

--- src/jp/co/wiss1/control/G0021Control.java
+++ src/jp/co/wiss1/control/G0021Control.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import jp.co.wiss1.common.Constants;
+import jp.co.wiss1.common.EncodingUtils;
 import jp.co.wiss1.model.G0021Model;
 
 @WebServlet("/G0021Contlrl")
@@ -18,34 +18,38 @@ public class G0021Control extends HttpServlet{
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 	throws IOException, ServletException{
 
-//		ブラウザの文字コードの調整
-		request.setCharacterEncoding(Constants.CHARACTER_ENCODING);
-		response.setContentType("text/html;charset="+ Constants.CHARACTER_ENCODING);
+		//ブラウザの文字コードの調整
+		EncodingUtils.responseEncoding(response);
 
-//		登録(INSERT)②の処理
+		//登録(INSERT)②の処理
 
-//		登録する項目を受け取る
+		//登録する項目を受け取る
 		String actressId = request.getParameter("actressId");
 		String actressName =request.getParameter("actressName");
 		String companyId =request.getParameter("companyId");
-		String contentsIdList  = request.getParameter("comtentsIdList") ;
+		boolean successFlag = true;
+		String[]  contentsIdList  = request.getParameterValues("comtentsIdList") ;
 
-//			登録する項目を送る
-		int flag = G0021Model.insertActress(actressId,actressName,companyId,contentsIdList);
+		for(int i = 1;i <= contentsIdList.length; i++){
+			//登録する項目を送る
+			int ret = G0021Model.insertActress(actressId,actressName,companyId,contentsIdList[i]);
+			if (ret == 0) {
+				successFlag = false;
+				break;
+			}
+		}
 
-//		Viewの画面に戻す
-		if(flag <= 1 ){
-//		 登録成功
-		 request.setAttribute("flag",11);
+		//Viewの画面に戻す
+		if(successFlag == true){
+			// 登録成功
+			request.setAttribute("insertFlag",1);
 	    }
 		else{
-//		 登録失敗
-		 request.setAttribute("flag",10);
+			//登録失敗
+			request.setAttribute("insertFlag",0);
 		}
 
 		RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0020View.jsp");
 		dispatch.forward(request, response);
-
-
 	}
-}
+}//修正
