c30fbb293612a142e8a89576738e978a2b300a66
WISS1_0065 <WISS1_0065@PC0065>
2015-06-19 11:43:29 +0900

    6/18修正　荒木

--- src/jp/co/wiss1/control/G0031Control.java
+++ src/jp/co/wiss1/control/G0031Control.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import jp.co.wiss1.common.EncodingUtils;
 import jp.co.wiss1.model.G0031Model;
 @WebServlet("/G0031Control")
 public class G0031Control extends HttpServlet{
@@ -16,21 +17,20 @@ public class G0031Control extends HttpServlet{
 		throws IOException, ServletException{
 
 		//ブラウザの文字コードの設定
-		request.setCharacterEncoding("Windows-31J");
-		response.setContentType("text/html;charset=Shift_JIS");
+		EncodingUtils.responseEncoding(response);
 
 		//insert処理
 		//フォーム入力を受け取る
-		String companyId = request.getParameter("company_id");
-		String companyName =request.getParameter("company_name");
-		String companyAdress = request.getParameter("company_adress");
+		String companyId = request.getParameter("companyId");
+		String companyName =request.getParameter("companyName");
+		String companyAdress = request.getParameter("companyAdress");
 
 		//Modelに引数を渡しinsert処理を実行させる
-		//int i =
+		int insertFlag =
 		G0031Model.insertCompany(companyId, companyName, companyAdress);
 
 		//Viewに渡すフラグを設定
-		//request.setAttribute("flag",i);
+		request.setAttribute("insertFlag",insertFlag);
 
 		//Viewにフラグを渡す
 		RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0031View.jsp");
