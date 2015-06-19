7f1f704c4acbfe00e7438974181db6ca2b6d8385
WISS1_0069 <WISS1_0069@PC0069>
2015-06-19 17:08:09 +0900

    G0022 update_t_actress倉島06/19

--- src/jp/co/wiss1/model/G0022Model.java
+++ src/jp/co/wiss1/model/G0022Model.java
@@ -11,11 +11,6 @@ public class G0022Model {
 	public static void main(String[] args)
 	{
 		// TODO 自動生成されたメソッド・スタブ
-		//getEmployeeList_SELECT("0" , "島" , "雅");
-		//getEmployeeList_INSERT("128" , "檜山" , "隼" , "20001001");
-		//getCompanyList(0,null);
-		//getPreferenceList();
-		//getContentsList();
 		updateActress("0","","0","0");
 	}
 
@@ -34,7 +29,7 @@ public class G0022Model {
         	statement = connection.createStatement();
 
             /* 自動コミットを無効にする */
-        	connection.setAutoCommit(false);
+        	connection.setAutoCommit(true);
 
 
             String sql = "UPDATE t_actress SET";
@@ -55,29 +50,6 @@ public class G0022Model {
 
             System.out.println(upDateCount + "行を更新しました。");
 
-
-
-            // テーブル照会結果を出力
-           /*while(rs0.next())
-           {
-
-            	map.add(new HashMap());
-
-
-            	map.get(i).put("所属会社ID", rs0.getInt("company_ID"));
-            	map.get(i).put("女優名", rs0.getString("actress_name"));
-            	map.get(i).put("女優ID", rs0.getString("actress_ID"));
-
-
-
-
-            	//リストに入ったかの確認
-            	System.out.println(map.get(i).get("所属会社ID"));
-    			System.out.println(map.get(i).get("女優名"));
-    			System.out.println(map.get(i).get("女優ID"));
-
-				i++;
-            }*/
 
         }
         catch (SQLException e)
