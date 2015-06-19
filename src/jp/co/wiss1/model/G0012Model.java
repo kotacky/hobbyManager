958a7ac2102ea7f3817ced9924887b449bdf45f8
WISS1_0069 <WISS1_0069@PC0069>
2015-06-19 17:10:43 +0900

    G0012 update t_employee倉島06/19

--- src/jp/co/wiss1/model/G0012Model.java
+++ src/jp/co/wiss1/model/G0012Model.java
@@ -11,22 +11,13 @@ public class G0012Model {
 	public static void main(String[] args)
 	{
 		// TODO 自動生成されたメソッド・スタブ
-		//getEmployeeList_SELECT("0" , "島" , "雅");
-		//getEmployeeList_INSERT("128" , "檜山" , "隼" , "20001001");
-		updateEmployee("126","倉島","雅也", "20000801");
-		//getCompanyList(0,null);
-		//getPreferenceList();
-		//getContentsList();
-		//getActressList();
-	}
+		updateEmployee("126","倉島","雅也", "20000801");}
 
 
 	//社員一覧UPDATEの実行！！！
 	public static int updateEmployee(String employeeId , String familyName , String firstName ,String birthDate) { //社員テーブル参照
 
-		//List<HashMap<String,String>> employeeList = new ArrayList<HashMap<String,String>>() ;
-		//ResultSet resultSet = null;
-    	Connection connection = null;
+		Connection connection = null;
     	Statement statement = null;
     	int upDateCount = 0;
 
@@ -60,25 +51,6 @@ public class G0012Model {
 
 
             System.out.println(upDateCount + "行を更新しました。");
-
-            // テーブル照会結果を出力
-            /*while(resultSet.next())
-           {
-        	   HashMap<String, String> employeeInfo = new HashMap<String, String>();
-        	   employeeInfo.put("社員ID", resultSet.getString("employee_id"));
-        	   employeeInfo.put("姓", resultSet.getString("employee_family_name"));
-        	   employeeInfo.put("名", resultSet.getString("employee_first_name"));
-        	   employeeInfo.put("生年月日", resultSet.getString("birth_date"));
-
-
-            	//リストに入ったかの確認
-        	   System.out.println(employeeInfo.get("社員ID"));
-        	   System.out.println(employeeInfo.get("姓"));
-        	   System.out.println(employeeInfo.get("名"));
-        	   System.out.println(employeeInfo.get("生年月日"));
-
-            }*/
-
 
         }
         catch (SQLException e)
