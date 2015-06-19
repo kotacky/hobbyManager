935c15ff6ee5b9089df7eec45e917b1519a786f0
WISS1_0067 <WISS1_0067@PC0067>
2015-06-19 10:41:11 +0900

    【6/18のG0020Controlのソースコードをアップロード】高橋豊_20150619

--- src/jp/co/wiss1/control/G0020Control.java
+++ src/jp/co/wiss1/control/G0020Control.java
@@ -11,95 +11,104 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import jp.co.wiss1.common.Constants;
+import jp.co.wiss1.common.EncodingUtils;
 import jp.co.wiss1.model.G0020Model;
 import jp.co.wiss1.model.G0021Model;
 
 @WebServlet("/G0020Contlrl")
 public class G0020Control extends HttpServlet{
 
-		 public void doPost(HttpServletRequest request, HttpServletResponse response)
-		 throws IOException, ServletException{
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException{
 
-//	     ブラウザの文字コードの調整
-		 request.setCharacterEncoding(Constants.CHARACTER_ENCODING);
-		 response.setContentType("text/html;charset="+ Constants.CHARACTER_ENCODING);
+		// ブラウザの文字コードの調整
+		EncodingUtils.responseEncoding(response);
 
-//		 どの処理を行うかの命令を受け取る
-	 	 String processDiv = request.getParameter("processDiv");
+		//どの処理を行うかの命令を受け取る
+		String processDiv = request.getParameter("processDiv");
 
-//		 処理に必要となる情報を受け取る
-		 String actressId = request.getParameter("actress_id");
-		 String actressName = request.getParameter("actress_name");
-		 String companyName = request.getParameter("company_name");
+		//処理に必要となる情報を受け取る
+		String actressId = request.getParameter("actressId");
+		String actressName = request.getParameter("actressName");
 
-//	 	 検索(SELECT)の処理
-	 	 if ("select".equals(processDiv)){
 
-//		   検索項目を渡し、リストを受け取る
-		   List <HashMap<String,String>> actressList = G0020Model.getActressList(actressId,actressName);
+		//検索(SELECT)の処理
+		if ("select".equals(processDiv)){
 
-//		   画面一覧へ結果を返す
-		   request.setAttribute("actressList",actressList);
-		   RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0020View.jsp");
-		   dispatch.forward(request, response);
-		 }
+			//検索項目を渡し、リストを受け取る
+			List <HashMap<String,String>> actressList = G0020Model.getActressList(actressId,actressName);
 
-//		 登録(INSERT)①の処理
-		 if ("insert".equals(processDiv)){
-
-//		   会社名のリストを受け取る
-		   List <HashMap<String,String>> colmncompanyList =G0021Model.getColumnCompnyList();
-
-//		   会社名をViewへ送る
-		   request.setAttribute("colmncompanyList",colmncompanyList);
-		   RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0021View.jsp");
-		   dispatch.forward(request, response);
-
-//		   G0021Controlへ
+			// 画面一覧へ結果を返す
+			request.setAttribute("actressList",actressList);
+			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0020View.jsp");
+			dispatch.forward(request, response);
 		 }
 
 
-//		 更新(UPDATE)①の処理
+		//登録(INSERT)①の処理
+		if ("insert".equals(processDiv)){
+
+			//会社名リストとコンテンツリストを受け取る
+			List <HashMap<String,String>> columncompanyList =G0021Model.getColumnCompanyList();
+			List <HashMap<String,String>> columncontentsList = G0020Model.getColumnContentsList();
+
+			// 会社名リストとコンテンツリストをViewへ送る
+			request.setAttribute("columncompanyList",columncompanyList);
+			request.setAttribute("columncontentsList",columncontentsList);
+			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0021View.jsp");
+			dispatch.forward(request, response);
+
+			//G0021Controlへ
+		 }
+
+
+		 //更新(UPDATE)①の処理
 		 if ("update".equals(processDiv)){
 
-//		   更新対象を検索し、リストを受け取る
-		   List <HashMap<String,String>> actressList = G0020Model.getActressList(actressId,"");
+			// 会社名リストとコンテンツリストを受け取る
+			List <HashMap<String,String>> colmncompanyList =G0021Model.getColumnCompanyList();
+			List <HashMap<String,String>> columncontentsList = G0020Model.getColumnContentsList();
 
-//		   更新対象のリストを送る
-		   request.setAttribute("actressList",actressList);
-		   RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0022View.jsp");
-		   dispatch.forward(request, response);
+			//会社名リストとコンテンツリストをViewへ送る
+			request.setAttribute("colmncompanyList",colmncompanyList);
+			request.setAttribute("colmncontentsList",columncontentsList);
+			//更新するリストを検索し、リストを受け取る
+			List <HashMap<String,String>> actressList = G0020Model.getActressList(actressId,"");
 
-//		   G0022Controlへ
+			//更新するリストをView送る
+			request.setAttribute("actressList",actressList);
+			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0022View.jsp");
+			dispatch.forward(request, response);
+
+			//G0022Controlへ
 		 }
 
 
-//		 削除(DELETE)の処理
+		 //削除(DELETE)の処理
 		 if ("delete".equals(processDiv)){
 
-//		   削除対象の主キーを受け取る
-		   String deleteId = request.getParameter("actressID");
+			// 削除対象の主キーを受け取る
+			String deleteId = request.getParameter("actressId");
 
-//		   削除対象の主キーを送る
-		   int flag = G0020Model.deleteActress (deleteId);
+			//削除対象の主キーを送る
+			int deleteFlag = G0020Model.deleteActress (deleteId);
 
 
-//		   削除後の項目を送り、リストを受け取る
-		   List <HashMap<String,String>> actressList = G0020Model.getActressList(actressId,actressName);
+			//削除の項目を送り、削除後のリストを受け取る
+			List <HashMap<String,String>> actressList = G0020Model.getActressList(actressId,actressName);
 
-//		   画面一覧へ結果を返す
-		   if(flag >= 1){
-//		    登録成功
-		    request.setAttribute("flag",31);
-		   }
-		   else{
-//		    登録失敗
-		    request.setAttribute("flag",30);
-		   }
-		   request.setAttribute("actressList",actressList);
-		   RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0020View.jsp");
-		   dispatch.forward(request, response);
-		}
+			//画面一覧へ結果を返す
+			if(deleteFlag == 1){
+			//登録成功
+			request.setAttribute("deleteFlag",deleteFlag);
+			}
+			else{
+			//登録失敗
+			request.setAttribute("deleteFlag",deleteFlag);
+			}
+			request.setAttribute("actressList",actressList);
+			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0020View.jsp");
+			dispatch.forward(request, response);
+		 }
 	}
 }
