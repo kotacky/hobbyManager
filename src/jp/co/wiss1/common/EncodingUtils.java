53529deef08172c8f7849d71d7b1b0c48a87c674
WISS1_0049 <WISS1_0049@PC_0049>
2015-06-19 15:46:25 +0900

    EncodingUtilsの引数にrequestを追加しました。エラーが出ます。次からののコミットで修正していきます。中村_0619

--- src/jp/co/wiss1/common/EncodingUtils.java
+++ src/jp/co/wiss1/common/EncodingUtils.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class EncodingUtils extends HttpServlet{
-	public static void responseEncoding(HttpServletResponse response)
+	public static void responseEncoding(HttpServletRequest request,HttpServletResponse response)
 		throws IOException, ServletException{
 			//ブラウザの文字コードで返す
-			response.setCharacterEncoding(Constants.CHARACTER_ENCODING);
+			request.setCharacterEncoding(Constants.CHARACTER_ENCODING);
 			response.setContentType("text/html;charset="+Constants.CHARACTER_ENCODING);
 	}
 }
