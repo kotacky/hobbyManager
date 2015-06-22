7283e07db3578bc86af47ef3d44e7a83ddb8c7e3
WISS1_0067 <WISS1_0067@PC0067>
2015-06-22 09:46:35 +0900

    modelに送る項目の順番修正　高橋0622

--- src/jp/co/wiss1/control/G0021Control.java
+++ src/jp/co/wiss1/control/G0021Control.java
@@ -32,7 +32,7 @@ public class G0021Control extends HttpServlet{
 
 		for(int i = 1;i <= contentsIdList.length; i++){
 			//登録する項目を送る
-			int ret = G0021Model.insertActress(actressId,actressName,companyId,contentsIdList[i]);
+			int ret = G0021Model.insertActress(companyId,actressName,actressId,contentsIdList[i]);
 			if (ret == 0) {
 				successFlag = false;
 				break;
