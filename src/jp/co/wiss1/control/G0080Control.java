3eff31ed94c27f3d08cd31f4b03005b1daec3c25
WISS1_0049 <WISS1_0049@PC_0049>
2015-09-08 15:03:21 +0900

    9月改修のため、変更しました。中村_0908

---
+++ src/jp/co/wiss1/control/G0080Control.java
@@ -0,0 +1,102 @@
+package jp.co.wiss1.control;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.wiss1.common.EncodingUtils;
+import jp.co.wiss1.model.G0020Model;
+import jp.co.wiss1.model.G0051Model;
+
+@WebServlet("/G0080Control")
+public class G0080Control extends HttpServlet{
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException{
+
+		//ブラウザの文字コードで返す
+		EncodingUtils.responseEncoding(request,response);
+
+		//Viewから処理命令を受け取る　
+		String processDiv = request.getParameter("processDiv");
+
+		//処理に必要な情報を受け取る
+		String employeeId = request.getParameter("employeeId");
+		String familyName = request.getParameter("employeeFamilyName");
+		String firstName = request.getParameter("employeeFirstName");
+
+		//検索の処理
+		if("select".equals(processDiv)){
+
+			//検索に必要なものを引数、検索結果のリストを戻り値としてメソッドを呼び出す。
+			List<HashMap<String, String>> preferenceList = G0080Model.getPreferenceList(employeeId, familyName, firstName);
+
+			//結果をViewに送る
+			request.setAttribute("preferenceList", preferenceList);
+
+			//検索条件保持のために送る
+			request.setAttribute("employeeList", employeeId);
+			request.setAttribute("actressList", familyName);
+			request.setAttribute("contentsList", firstName);
+			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0080View.jsp");
+			dispatch.forward(request, response);
+		}
+
+		//登録①の処理
+		if("insert".equals(processDiv)){
+
+			//登録画面に必要な要素を引き出す
+			List<HashMap<String, String>> employeeList = G0051Model.getColumnEmployeeList();
+			List<HashMap<String, String>> actressList = G0051Model.getColumnActressList();
+			List<HashMap<String, String>> contentsList = G0020Model.getColumnContentsList();
+
+			//登録画面にプルダウンで必要なもの送る
+			request.setAttribute("employeeList", employeeList);
+			request.setAttribute("actressList", actressList);
+			request.setAttribute("contentsList", contentsList);
+			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0051View.jsp");
+			dispatch.forward(request, response);
+		}
+		//削除の処理
+		if("delete".equals(processDiv)){
+
+			//処理に必要な情報を受け取る
+			String deleteEmployeeId = request.getParameter("radioButton");
+
+			//radioButtoがnullでないならば処理を行う
+			if(deleteEmployeeId != null){
+
+				//デリートのメソッドを呼ぶ
+				int deleteFlag = G0080Model.deletePreference(deleteEmployeeId);
+
+				//デリート後のリストと削除処理のフラグを送る
+				if(deleteFlag >= 1){
+					request.setAttribute("deleteFlag", 1);
+				}else{
+					request.setAttribute("deleteFlag",deleteFlag);
+				}
+			}else{
+				//nullのとき処理を行わずに返す
+				request.setAttribute("deleteFlag",0);
+			}
+			//デリート後のリストを検索メソッドで取り出す
+			List<HashMap<String, String>> preferenceList = G0080Model.getPreferenceList(employeeId, familyName, firstName);
+
+			//削除処理後のリストを送る
+			request.setAttribute("preferenceList", preferenceList);
+
+			//検索条件保持のために送る
+			request.setAttribute("employeeList", employeeId);
+			request.setAttribute("actressList", familyName);
+			request.setAttribute("contentsList", firstName);
+			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0080View.jsp");
+			dispatch.forward(request, response);
+		}
+	}
+}
