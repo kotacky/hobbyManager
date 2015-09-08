d6268592980736faa72fd438eea845f310c6e602
WISS1_0049 <WISS1_0049@PC_0049>
2015-09-08 15:05:15 +0900

    9月改修のため、変更しました。

---
+++ src/jp/co/wiss1/control/G0081Control.java
@@ -0,0 +1,36 @@
+package jp.co.wiss1.control;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.wiss1.common.EncodingUtils;
+
+@WebServlet("/G0081Control")
+public class G0081Control extends HttpServlet{
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException{
+
+		//ブラウザの文字コードで返す
+		EncodingUtils.responseEncoding(request,response);
+
+		//登録に必要な情報を受け取る
+		String employeeId = request.getParameter("employeeId");
+		String actressId = request.getParameter("actressId");
+
+		//登録する項目を送る
+		int insertFlag = G0081Model.insertPreference(employeeId,actressId);
+
+		//Viewの画面に戻す
+		//登録成功なら1、失敗なら0がViewへ
+		request.setAttribute("insertFlag",insertFlag);
+
+		RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0050View.jsp");
+		dispatch.forward(request, response);
+	}
+}
