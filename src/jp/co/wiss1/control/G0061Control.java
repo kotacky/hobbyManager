aad2947a179c622a78c0d9fa4f5778130a9c86fa
WISS1_0034 <WISS1_0034@PC0034>
2015-09-11 16:27:33 +0900

    G0061C改修　高島_0911

---
+++ src/jp/co/wiss1/control/G0061Control.java
@@ -0,0 +1,39 @@
+package jp.co.wiss1.control;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jp.co.wiss1.common.EncodingUtils;
+import jp.co.wiss1.model.G0061Model;
+
+@WebServlet("/G0061Control")
+public class G0061Control extends HttpServlet{
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException{
+
+		//高島史裕
+		//ブラウザの文字コードで返す
+		EncodingUtils.responseEncoding(request,response);
+
+		//登録に必要な情報を受け取る
+		String movieId = request.getParameter("movieId");
+		String title = request.getParameter("movieName");
+		String releaseDate = request.getParameter("releaseDate");
+
+		//登録のメソッドを呼び出す
+		int insertFlag = G0061Model.insertMovie(movieId, title, releaseDate);
+
+		//登録完了フラグを送る
+		request.setAttribute("insertFlag",insertFlag);
+
+		RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0061View.jsp");
+		dispatch.forward(request, response);
+
+	}
+}
