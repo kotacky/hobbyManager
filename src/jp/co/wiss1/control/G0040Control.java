7039c99371aa577816e56445b8ceee9809177a1a
WISS1_0049 <WISS1_0049@PC_0049>
2015-09-11 14:28:35 +0900

    コメント文と処理内容が一致してなかったため修正しました。中村_0910

--- src/jp/co/wiss1/control/G0040Control.java
+++ src/jp/co/wiss1/control/G0040Control.java
@@ -37,12 +37,12 @@ public class G0040Control extends HttpServlet{
 			List<HashMap<String, String>> contentsList = G0040Model.getContentsList(contentsId, title, broadCast);
 
 			//検索結果をViewに送る
-			request.setAttribute("contentsId", contentsId);
+			request.setAttribute("contentsList", contentsList);
 
 			//検索条件保持のために送る
 			request.setAttribute("title", title);
 			request.setAttribute("broadCast", broadCast);
-			request.setAttribute("contentsList", contentsList);
+			request.setAttribute("contentsId", contentsId);
 			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0040View.jsp");
 			dispatch.forward(request, response);
 		}
