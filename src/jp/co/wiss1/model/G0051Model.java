4eb46e2b7b7ab2127611de096dc363412a846e3e
WISS1_0067 <WISS1_0067@PC0067>
2015-09-11 09:41:35 +0900

    G0051_改修_那須直樹_0911

--- src/jp/co/wiss1/model/G0051Model.java
+++ src/jp/co/wiss1/model/G0051Model.java
@@ -1,27 +1,22 @@
 package jp.co.wiss1.model;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import jp.co.wiss1.common.DBAccessUtils;
 
-public class G0051Model {
+public class G0091Model {
 
 	public static void main(String args[])
 	{
 
-		insertPreference("128" , "0");
-		getColumnEmployeeList();
-		getColumnActressList();
+		insertdrama("" , "子ずれ狼" , "大河ドラマ");
+
 	}
 
 
-	public static int insertPreference(String employeeId , String actressId) { //女優テーブル参照
+	public static int insertdrama(String dramaId , String dramaTitle , String  broadcastCool) { //ドラマテーブル参照
 
 		//各変数を宣言、初期化
     	Connection connection = null;
@@ -39,14 +34,17 @@ public class G0051Model {
         	connection.setAutoCommit(true);
 
         	//主キーが入力されなかったとき、SQL文を実行しない
-        	 String insertSql = "INSERT INTO t_preference";
+        	 String insertSql = "INSERT INTO t_drama";
              System.out.println("1:" + insertSql);
 
-             if(!"".equals(employeeId))
+             if(!"".equals(dramaId))
              {
-             	insertSql = insertSql + "(employee_id, actress_id)"
-             			+ "VALUES('" + employeeId + "'"
-						+ ",'" + actressId + "')";
+             	insertSql = insertSql + "(drama_id"
+             			+ ",drama_title"
+             			+ ",broadcasting_cool)"
+             			+ "VALUES('" + dramaId + "'"
+             			+ ",'" + dramaTitle + "'"
+ 						+ ",'" +  broadcastCool + "')";
 
              	insertCount = statement.executeUpdate(insertSql);
              }
@@ -58,10 +56,13 @@ public class G0051Model {
              //String sql0 = "INSERT INTO t_actress (company_id , actress_name , actress_id)"
              //+ " VALUES ('" + cid + "', '" + n + "','" + aid + "')";
 
-             System.out.println("引数に" + employeeId + "が入力されました。");
-             System.out.println("引数に" + actressId + "が入力されました。");
+             System.out.println("引数に" + dramaId + "が入力されました。");
+             System.out.println("引数に" + dramaTitle + "が入力されました。");
+             System.out.println("引数に" + broadcastCool + "が入力されました。");
 
              System.out.println(insertSql);
+
+             //rs0 = stmt0.executeQuery (sql0);
 
              //影響のあった行数を出力
 
@@ -89,139 +90,6 @@ public class G0051Model {
          }
         return insertCount;
  	}
-
-
-	public static List<HashMap<String,String>> getColumnEmployeeList() //プルダウン表示に必要な社員データを送るメソッド
-	{
-		List<HashMap<String,String>> companyColumnList = new ArrayList<HashMap<String,String>>() ;//DBから取得したデータを詰める
-
-		//各変数を宣言、初期化
-		ResultSet resultSet = null;
-    	Connection connection = null;
-    	Statement statement = null;
-
-
-        try
-        {
-            // テーブル照会実行
-        	connection = DBAccessUtils.getConnection();
-        	statement = connection.createStatement();
-
-            //自動コミットを無効にする
-        	connection.setAutoCommit(false);
-
-        	//プルダウン用にカラム名を取得
-            String columnSelectSql = "SELECT employee_id , employee_family_name , employee_first_name FROM t_employee";
-            System.out.println("1:" + columnSelectSql);
-
-            resultSet = statement.executeQuery (columnSelectSql);
-
-           // テーブル照会結果を出力
-           while(resultSet.next())
-           {
-            	//社員情報を作成する
-            	HashMap<String, String> employeeInfo = new HashMap<String, String>();
-            	employeeInfo.put("employeeId", resultSet.getString("employee_id"));
-            	employeeInfo.put("employeeFamilyName", resultSet.getString("employee_family_name"));
-            	employeeInfo.put("employeeFirstName", resultSet.getString("employee_first_name"));
-
-            	//社員情報をリストに追加する
-            	companyColumnList.add(employeeInfo);
-
-            	//リストに入ったかの確認
-            	System.out.println(employeeInfo.get("employeeId"));
-            	System.out.println(employeeInfo.get("employeeFamilyName"));
-            	System.out.println(employeeInfo.get("employeeFirstName"));
-			}
-
-        }
-        catch (SQLException e)
-        {
-            System.err.println("SQL failed.");
-            e.printStackTrace ();
-        }
-        finally
-        {
-			// データベースのクローズ
-        	try
-        	{
-                //resultSet.close();
-                statement.close();
-                connection.close();
-        	}
-        	catch (Exception e)
-        	{
-        		System.out.println("Close failed.");
-        	}
-        	System.out.println("一覧取得処理終了");
-        }
-        return companyColumnList;
-	}
-
-
-	public static List<HashMap<String,String>> getColumnActressList() //プルダウン表示に必要な女優データを送るメソッド
-	{
-		List<HashMap<String,String>> companyColumnList = new ArrayList<HashMap<String,String>>() ;//DBから取得したデータを詰める
-
-		//各変数を宣言、初期化
-		ResultSet resultSet = null;
-		Connection connection = null;
-		Statement statement = null;
-
-
-	    try
-	    {
-	        // テーブル照会実行
-	    	connection = DBAccessUtils.getConnection();
-	    	statement = connection.createStatement();
-
-	        //自動コミットを無効にする
-	    	connection.setAutoCommit(false);
-
-	    	//プルダウン用にカラム名を取得
-	        String columnSelectSql = "SELECT distinct actress_id , actress_name FROM t_actress ORDER BY actress_id";
-	        System.out.println("1:" + columnSelectSql);
-
-	        resultSet = statement.executeQuery (columnSelectSql);
-
-	       // テーブル照会結果を出力
-	       while(resultSet.next())
-	       {
-	        	//社員情報を作成する
-	        	HashMap<String, String> actressInfo = new HashMap<String, String>();
-	        	actressInfo.put("actressId", resultSet.getString("actress_id"));
-	        	actressInfo.put("actressName", resultSet.getString("actress_name"));
-
-	        	//社員情報をリストに追加する
-	        	companyColumnList.add(actressInfo);
-
-	        	//リストに入ったかの確認
-	        	System.out.println(actressInfo.get("actressId"));
-	        	System.out.println(actressInfo.get("actressName"));
-
-			}
-
-	    }
-	    catch (SQLException e)
-	    {
-	        System.err.println("SQL failed.");
-	        e.printStackTrace ();
-	    }
-	    finally
-	    {
-			// データベースのクローズ
-	    	try
-	    	{
-	            //resultSet.close();
-	            statement.close();
-	            connection.close();
-	    	}
-	    	catch (Exception e)
-	    	{
-	    		System.out.println("Close failed.");
-	    	}
-	    	System.out.println("一覧取得処理終了");
-	    }
-	    return companyColumnList;
-	}
 }
+
+//ぼくのサーヴァントは最強なんだ！(集中線)
