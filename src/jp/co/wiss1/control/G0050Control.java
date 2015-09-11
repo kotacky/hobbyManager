b3af1131b5d1b26ca2aa816faea5c1e7db8dcf4f
WISS1_0067 <WISS1_0067@PC0067>
2015-09-11 16:30:29 +0900

    G0050_改修_那須直樹_0911

--- src/jp/co/wiss1/control/G0050Control.java
+++ src/jp/co/wiss1/control/G0050Control.java
@@ -12,9 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import jp.co.wiss1.common.EncodingUtils;
-import jp.co.wiss1.model.G0020Model;
 import jp.co.wiss1.model.G0050Model;
-import jp.co.wiss1.model.G0051Model;
 
 @WebServlet("/G0050Control")
 public class G0050Control extends HttpServlet{
@@ -24,78 +22,87 @@ public class G0050Control extends HttpServlet{
 		//ブラウザの文字コードで返す
 		EncodingUtils.responseEncoding(request,response);
 
-		//Viewから処理命令を受け取る　
+		//Viewから処理命令を受け取る
 		String processDiv = request.getParameter("processDiv");
 
 		//処理に必要な情報を受け取る
-		String employeeId = request.getParameter("employeeId");
-		String familyName = request.getParameter("employeeFamilyName");
-		String firstName = request.getParameter("employeeFirstName");
+		String dramaId = request.getParameter("dramaId");
+		String title = request.getParameter("dramaName");
+		String broadcastCool = request.getParameter("broadcastCool");
 
 		//検索の処理
 		if("select".equals(processDiv)){
 
 			//検索に必要なものを引数、検索結果のリストを戻り値としてメソッドを呼び出す。
-			List<HashMap<String, String>> preferenceList = G0050Model.getPreferenceList(employeeId, familyName, firstName);
+			List<HashMap<String, String>> dramaList = G0050Model.getDramaList(dramaId, title, broadcastCool);
 
-			//結果をViewに送る
-			request.setAttribute("preferenceList", preferenceList);
+			//検索結果をViewに送る
+			request.setAttribute("dramaList", dramaList);
 
 			//検索条件保持のために送る
-			request.setAttribute("employeeList", employeeId);
-			request.setAttribute("actressList", familyName);
-			request.setAttribute("contentsList", firstName);
+			request.setAttribute("dramaName", title);
+			request.setAttribute("broadcastCool", broadcastCool);
+			request.setAttribute("dramaId", dramaId);
 			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0050View.jsp");
 			dispatch.forward(request, response);
 		}
 
-		//登録①の処理
-		if("insert".equals(processDiv)){
+		//更新①の処理
+		if("update".equals(processDiv)){
 
-			//登録画面に必要な要素を引き出す
-			List<HashMap<String, String>> employeeList = G0051Model.getColumnEmployeeList();
-			List<HashMap<String, String>> actressList = G0051Model.getColumnActressList();
-			List<HashMap<String, String>> contentsList = G0020Model.getColumnContentsList();
+			//更新前の情報を引き出すための主キーを受け取る
+			String updateDramaId = request.getParameter("radioButton");
 
-			//登録画面にプルダウンで必要なもの送る
-			request.setAttribute("employeeList", employeeList);
-			request.setAttribute("actressList", actressList);
-			request.setAttribute("contentsList", contentsList);
-			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0051View.jsp");
-			dispatch.forward(request, response);
+			//radioButtoがnullでないならば処理を行う
+			if(updateDramaId != null){
+
+				//更新前の情報を検索メソッドで受け取る
+				List<HashMap<String, String>> dramaList = G0050Model.getDramaList(updateDramaId, "", "");
+
+				//更新前の情報を更新ページに飛ばす
+				request.setAttribute("dramaList", dramaList);
+				RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0052View.jsp");
+				dispatch.forward(request, response);
+			}else{
+				//nullのとき処理を行わずに返す
+				int updateFlag = 0;
+				request.setAttribute("updateFlag",updateFlag);
+				RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0050View.jsp");
+				dispatch.forward(request, response);
+			}
+
 		}
+
 		//削除の処理
 		if("delete".equals(processDiv)){
 
-			//処理に必要な情報を受け取る
-			String deleteEmployeeId = request.getParameter("radioButton");
+			//削除に必要な情報を受け取る
+			String deleteDramaId = request.getParameter("radioButton");
 
 			//radioButtoがnullでないならば処理を行う
-			if(deleteEmployeeId != null){
+			if(deleteDramaId != null){
 
 				//デリートのメソッドを呼ぶ
-				int deleteFlag = G0050Model.deletePreference(deleteEmployeeId);
+				int deleteFlag = G0050Model.deleteDrama(deleteDramaId);
 
-				//デリート後のリストと削除処理のフラグを送る
-				if(deleteFlag >= 1){
-					request.setAttribute("deleteFlag", 1);
-				}else{
+				//デリート後のリストと削除完了のフラグを送る
+
 					request.setAttribute("deleteFlag",deleteFlag);
-				}
+
 			}else{
 				//nullのとき処理を行わずに返す
 				request.setAttribute("deleteFlag",0);
 			}
 			//デリート後のリストを検索メソッドで取り出す
-			List<HashMap<String, String>> preferenceList = G0050Model.getPreferenceList(employeeId, familyName, firstName);
+			List<HashMap<String, String>> dramaList = G0050Model.getDramaList(dramaId, title, broadcastCool);
 
 			//削除処理後のリストを送る
-			request.setAttribute("preferenceList", preferenceList);
+			request.setAttribute("dramaList", dramaList);
 
 			//検索条件保持のために送る
-			request.setAttribute("employeeList", employeeId);
-			request.setAttribute("actressList", familyName);
-			request.setAttribute("contentsList", firstName);
+			request.setAttribute("dramaId", dramaId);
+			request.setAttribute("dramaName", title);
+			request.setAttribute("broadCast", broadcastCool);
 			RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0050View.jsp");
 			dispatch.forward(request, response);
 		}
