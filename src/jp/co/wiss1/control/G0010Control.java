8d826ccf5c973b2b457c43e3ae2f05ba0ae7b7ab
WISS1_0068 <WISS1_0068@PC0068>
2015-11-04 13:59:30 +0900

    修正分11/4　14：00鈴木

--- src/jp/co/wiss1/control/G0010Control.java
+++ src/jp/co/wiss1/control/G0010Control.java
@@ -23,7 +23,6 @@ public class G0010Control extends HttpServlet{
 
 		//Viewから処理命令を受け取る
 		String processDiv = request.getParameter("processDiv");
-		String employeeAuthority = request.getParameter("employeeAuthority");
 		//処理に必要な情報を受け取る
 		String employeeId = request.getParameter("employeeId");
 		String familyName = request.getParameter("employeeFamilyName");
@@ -38,7 +37,6 @@ public class G0010Control extends HttpServlet{
 
 			//検索結果をViewに送る
 			request.setAttribute("employeeList", employeeList);
-			request.setAttribute("employeeAuthority", employeeAuthority);
 
 			//検索条件保持のために送る
 			request.setAttribute("employeeId", employeeId);
@@ -61,14 +59,12 @@ public class G0010Control extends HttpServlet{
 				List<HashMap<String, String>> employeeList = G0010Model.getEmployeeList(updateEmployeeId, "", "");
 
 				//更新前の情報を更新ページに飛ばす
-				request.setAttribute("employeeAuthority", employeeAuthority);
 				request.setAttribute("employeeList", employeeList);
 				RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0012View.jsp");
 				dispatch.forward(request, response);
 			}else{
 				//nullのとき処理を行わずに返す
 				int updateFlag = 0;
-				request.setAttribute("employeeAuthority", employeeAuthority);
 				request.setAttribute("updateFlag",updateFlag);
 				RequestDispatcher dispatch =getServletContext().getRequestDispatcher("/view/G0010View.jsp");
 				dispatch.forward(request, response);
@@ -103,7 +99,6 @@ public class G0010Control extends HttpServlet{
 
 			//削除処理後のリストを送る
 			request.setAttribute("employeeList",employeeList);
-			request.setAttribute("employeeAuthority", employeeAuthority);
 
 			//検索条件保持のために送る
 			request.setAttribute("employeeId", employeeId);
