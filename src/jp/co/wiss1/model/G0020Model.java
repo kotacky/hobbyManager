6a0f0d52b5dd347107aed742afd975c7c64d3a13
WISS1_0067 <WISS1_0067@PC0067>
2015-12-08 11:16:53 +0900

    G0020Model_改修_20151208

--- src/jp/co/wiss1/model/G0020Model.java
+++ src/jp/co/wiss1/model/G0020Model.java
@@ -42,7 +42,7 @@ public class G0020Model
 
 
 	        String sql = "SELECT distinct t_actress.actress_id, t_actress.actress_name, t_company.company_name ,t_actress.birth_date ,t_actress.blood_type, t_actress.birth_place "
-        		     +"FROM "
+	        		 +"FROM "
         	         +"t_actress LEFT OUTER JOIN t_company ON t_actress.company_id = t_company.company_id where ";
 
 	        if(!"".equals(actressId)) {
