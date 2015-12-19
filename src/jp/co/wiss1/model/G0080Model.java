f8818edb05b3413ebbe36f54e89fc70aa499818e
WISS1_0082 <WISS1_0082@PC0082>
2015-12-19 14:30:10 +0900

    保科改修_Viewリンク修正

--- src/jp/co/wiss1/model/G0080Model.java
+++ src/jp/co/wiss1/model/G0080Model.java
@@ -31,84 +31,83 @@ public class G0080Model {
 			List<HashMap<String, String>> preferList = new ArrayList<HashMap<String, String>>() ;
 
 			// テーブル照会実行
-	        try{
-	        	//データベースへ接続
-		    	connection = DBAccessUtils.getConnection();
-		    	//Statementを取得するためのコード
-		    	statement = connection.createStatement();
-		    	//自動コミットを無効にする
-		        connection.setAutoCommit(false);
+			try{
+				//データベースへ接続
+				connection = DBAccessUtils.getConnection();
+				//Statementを取得するためのコード
+				statement = connection.createStatement();
+				//自動コミットを無効にする
+				connection.setAutoCommit(false);
 
 
-		        String sql = "SELECT distinct t_employee.employee_id,"
-		        				+"t_employee.employee_family_name, "
-		        				+"t_employee.employee_first_name, "
-		        				+"t_actress.actress_id, "
-		        				+"t_actress.actress_name, "
-		        				+"COALESCE(magazine_name,'未登録') AS magazine_name, "
-		        				+"COALESCE(drama_title,'未登録') AS drama_title, "
-		        				+"COALESCE(movie_title,'未登録') AS movie_title, "
-		        				+"COALESCE(commercial_name,'未登録') AS commercial_name "
-		        				+"FROM "
-		        				+"t_preference "
-		        				+"LEFT OUTER JOIN t_actress ON t_preference.actress_id = t_actress.actress_id "
-		        				+"LEFT OUTER JOIN t_dramas ON t_actress.drama_id = t_dramas.drama_id "
-		        				+"LEFT OUTER JOIN t_movie ON t_actress.movie_id = t_movie.movie_id "
-		        				+"LEFT OUTER JOIN t_magazine ON t_actress.magazine_id = t_magazine.magazine_id "
-		        				+"LEFT OUTER JOIN t_commercial ON t_actress.commercial_id = t_commercial.commercial_id "
-		        				+"LEFT OUTER JOIN t_employee ON t_preference.employee_id = t_employee.employee_id where ";
+				String sql = "SELECT distinct t_employee.employee_id,"
+						+"t_employee.employee_family_name, "
+						+"t_employee.employee_first_name, "
+						+"t_actress.actress_id, "
+						+"COALESCE(actress_name,'未登録') AS actress_name, "
+						+"COALESCE(magazine_name,'未登録') AS magazine_name, "
+						+"COALESCE(drama_title,'未登録') AS drama_title, "
+						+"COALESCE(movie_title,'未登録') AS movie_title, "
+						+"COALESCE(commercial_name,'未登録') AS commercial_name "
+						+"FROM "
+						+"t_preference "
+						+"LEFT OUTER JOIN t_actress ON t_preference.actress_id = t_actress.actress_id "
+						+"LEFT OUTER JOIN t_dramas ON t_actress.drama_id = t_dramas.drama_id "
+						+"LEFT OUTER JOIN t_movie ON t_actress.movie_id = t_movie.movie_id "
+						+"LEFT OUTER JOIN t_magazine ON t_actress.magazine_id = t_magazine.magazine_id "
+						+"LEFT OUTER JOIN t_commercial ON t_actress.commercial_id = t_commercial.commercial_id "
+						+"LEFT OUTER JOIN t_employee ON t_preference.employee_id = t_employee.employee_id where ";
 
-		        System.out.println(sql);
+				System.out.println(sql);
 
-		        if(!"".equals(employeeId)) {
-		        	sql = sql + "t_employee.employee_id = '"+ employeeId +"' AND ";
-		        }
-		        if(!"".equals(familyName)) {
-		        	sql = sql + "t_employee.employee_family_name like '%"+ familyName +"%' AND ";
-		        }
-	        	sql = sql + "t_employee.employee_first_name like '%"+ firstName +"%'";
-	        	//sql文終了
-	        	sql = sql + "ORDER BY employee_id";
+				if(!"".equals(employeeId)) {
+					sql = sql + "t_employee.employee_id = '"+ employeeId +"' AND ";
+				}
+				if(!"".equals(familyName)) {
+					sql = sql + "t_employee.employee_family_name like '%"+ familyName +"%' AND ";
+				}
+				sql = sql + "t_employee.employee_first_name like '%"+ firstName +"%'";
+				//sql文終了
+				sql = sql + "ORDER BY employee_id";
 
 
-	            System.out.println("引数に" + employeeId + "が入力されました。");
-	            System.out.println("引数に" + familyName + "が入力されました。");
-	            System.out.println("引数に" + firstName + "が入力されました。");
-	            System.out.println(sql);
+				System.out.println("引数に" + employeeId + "が入力されました。");
+				System.out.println("引数に" + familyName + "が入力されました。");
+				System.out.println("引数に" + firstName + "が入力されました。");
+				System.out.println(sql);
 
 	          //SELECT文を実行するコード
-	            resultSet = statement.executeQuery(sql);
-	            System.out.println(resultSet);
+				resultSet = statement.executeQuery(sql);
+				System.out.println(resultSet);
 
 	          //SELECT文の結果を参照
-	            while(resultSet.next()) {
+				while(resultSet.next()) {
+					HashMap<String, String> preferenceInfo = new HashMap<String, String>();
+					preferenceInfo.put("employeeId", resultSet.getString("employee_id"));
+					preferenceInfo.put("employeeFamilyName", resultSet.getString("employee_family_name"));
+					preferenceInfo.put("employeeFirstName", resultSet.getString("employee_first_name"));
+					preferenceInfo.put("actressName", resultSet.getString("actress_name"));
+					preferenceInfo.put("magazineName", resultSet.getString("magazine_name"));
+					preferenceInfo.put("dramaName", resultSet.getString("drama_title"));
+					preferenceInfo.put("movieName", resultSet.getString("movie_title"));
+					preferenceInfo.put("commercialName", resultSet.getString("commercial_name"));
+					preferenceInfo.put("actressId", resultSet.getString("actress_id"));
 
-	        	   HashMap<String, String> preferenceInfo = new HashMap<String, String>();
-	        	   preferenceInfo.put("employeeId", resultSet.getString("employee_id"));
-	        	   preferenceInfo.put("employeeFamilyName", resultSet.getString("employee_family_name"));
-	        	   preferenceInfo.put("employeeFirstName", resultSet.getString("employee_first_name"));
-	        	   preferenceInfo.put("actressName", resultSet.getString("actress_name"));
-	        	   preferenceInfo.put("magazineName", resultSet.getString("magazine_name"));
-	        	   preferenceInfo.put("dramaName", resultSet.getString("drama_title"));
-	        	   preferenceInfo.put("movieName", resultSet.getString("movie_title"));
-	        	   preferenceInfo.put("commercialName", resultSet.getString("commercial_name"));
-	        	   preferenceInfo.put("actressId", resultSet.getString("actress_id"));
 
-
-	        	   preferenceList.add(preferenceInfo);
+					preferenceList.add(preferenceInfo);
 
 	        	   //リストに入ったかの確認
-	        	   System.out.println(preferenceInfo.get("employeeId"));
-	        	   System.out.println(preferenceInfo.get("employeeFamilyName"));
-	        	   System.out.println(preferenceInfo.get("employeeFirstName"));
-	        	   System.out.println(preferenceInfo.get("actressId"));
-	        	   System.out.println(preferenceInfo.get("actressName"));
-	        	   System.out.println(preferenceInfo.get("magazineName"));
-	        	   System.out.println(preferenceInfo.get("dramaName"));
-	        	   System.out.println(preferenceInfo.get("movieName"));
-	        	   System.out.println(preferenceInfo.get("commercialName"));
+					System.out.println(preferenceInfo.get("employeeId"));
+					System.out.println(preferenceInfo.get("employeeFamilyName"));
+					System.out.println(preferenceInfo.get("employeeFirstName"));
+					System.out.println(preferenceInfo.get("actressId"));
+					System.out.println(preferenceInfo.get("actressName"));
+					System.out.println(preferenceInfo.get("magazineName"));
+					System.out.println(preferenceInfo.get("dramaName"));
+					System.out.println(preferenceInfo.get("movieName"));
+					System.out.println(preferenceInfo.get("commercialName"));
 
-	            }
+				}
 				for(int i = 1; i <= preferenceList.size(); i++){
 					String magazineName, dramaTitle, movieTitle, commercialName;
 					//HashMapを作る
